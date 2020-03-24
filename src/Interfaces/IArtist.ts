f3e837db711ad962a7b5f9d8ddf01e282a647b5c
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-03-24 14:57:04 +0200

    Add Artist Interface

---
+++ src/Interfaces/IArtist.ts
@@ -0,0 +1,15 @@
+export interface IArtist {
+  id: Number;
+  name: string;
+  link: string;
+  share: string;
+  picture: string;
+  picture_small: string;
+  picture_medium: string;
+  picture_big: string;
+  picture_xl: string;
+  nb_album: Number;
+  nb_fan: Number;
+  radio: boolean;
+  tracklist: string;
+}
