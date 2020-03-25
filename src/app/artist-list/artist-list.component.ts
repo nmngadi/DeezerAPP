94b93661cb7ea477708b5981d2f3168e7bd33cbc
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-03-25 11:12:12 +0200

    Add artist list component

--- src/app/artist-list/artist-list.component.ts
+++ src/app/artist-list/artist-list.component.ts
@@ -24,26 +24,11 @@ export class ArtistListComponent {
   constructor(private artistService: ArtistService) {}
 
   searchArtist() {
-    this.artistService
-      .getArtistList(this.searchstring)
-      .pipe(take(0))
-      .subscribe({
-        next: artists => {
-          this.artists = artists;
-        },
-        error: err => (this.errorMessage = err)
-      });
+    this.artistService.getArtistList(this.searchstring).subscribe({
+      next: artists => {
+        this.artists = artists;
+      },
+      error: err => (this.errorMessage = err)
+    });
   }
-
-  /*   ngOnChanges(changes: SimpleChanges) {
-    if (typeof changes["searchstring"] !== "undefined") {
-      // retrieve the quiz variable change info
-      var change = changes["searchstring"];
-
-      // only perform the task if the value has been changed
-      if (!change.isFirstChange()) {
-        // execute the Http request and retrieve the result
-        this.searchArtist();
-      }
-    } */
 }
