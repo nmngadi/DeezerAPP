7ea316b8bcf542da2a1a4eeef4bd36b68c8c4b81
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-03-25 16:40:49 +0200

    Add PWA changes to APP module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,8 @@ import { FormsModule } from "@angular/forms";
 //import { WelcomeComponent } from "./welcome/welcome.component";
 import { ArtistDetailsComponent } from "./artist-details/artist-details.component";
 import { RouterModule } from "@angular/router";
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -27,12 +27,14 @@ import { environment } from '../environments/environment';
     FormsModule,
     RouterModule.forRoot([
       { path: "artist", component: ArtistListComponent },
-      { path: "artist/:id", component: ArtistDetailsComponent }
-      // { path: "welcome", component: WelcomeComponent },
-      // { path: "", redirectTo: "welcome", pathMatch: "full" },
-      // { path: "**", redirectTo: "welcome", pathMatch: "full" }
+      { path: "artist/:id", component: ArtistDetailsComponent },
+
+      { path: "", redirectTo: "artist", pathMatch: "full" },
+      { path: "**", redirectTo: "artist", pathMatch: "full" }
     ]),
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
