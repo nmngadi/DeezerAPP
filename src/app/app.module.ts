810f1230df6c2c6db8c9c7aeed899951c6f48fd3
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-03-25 15:21:32 +0200

    Add Artist Details Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { ArtistListComponent } from "./artist-list/artist-list.component";
+import { FormsModule } from "@angular/forms";
+//import { WelcomeComponent } from "./welcome/welcome.component";
+import { ArtistDetailsComponent } from "./artist-details/artist-details.component";
+import { RouterModule } from "@angular/router";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistListComponent,
+    //  WelcomeComponent,
+    ArtistDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: "artist", component: ArtistListComponent },
+      { path: "artist/:id", component: ArtistDetailsComponent }
+      // { path: "welcome", component: WelcomeComponent },
+      // { path: "", redirectTo: "welcome", pathMatch: "full" },
+      // { path: "**", redirectTo: "welcome", pathMatch: "full" }
+    ]),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
