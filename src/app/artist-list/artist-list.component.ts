a8582e116d7b21478aad8387a9c2a229588b56ff
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-03-29 22:27:01 +0200

    Remove unused variables and exports

--- src/app/artist-list/artist-list.component.ts
+++ src/app/artist-list/artist-list.component.ts
@@ -1,30 +1,22 @@
-import {
-  Component,
-  OnInit,
-  Input,
-  SimpleChanges,
-  OnChanges
-} from '@angular/core';
+import { Component } from '@angular/core';
 import { IArtist } from 'src/Interfaces/IArtist';
 import { ArtistService } from 'src/services/artist.service';
-import { take } from 'rxjs/operators';
+
 @Component({
   selector: 'app-artist-list',
   templateUrl: './artist-list.component.html',
   styleUrls: ['./artist-list.component.css']
 })
 export class ArtistListComponent {
-  imageWidth = 50;
-  ImageMargin = 2;
   errorMessage = '';
   artists: IArtist[] = [];
 
-  searchstring: string;
+  searchString: string;
 
   constructor(private artistService: ArtistService) {}
 
   searchArtist() {
-    this.artistService.getArtistList(this.searchstring).subscribe({
+    this.artistService.getArtistList(this.searchString).subscribe({
       next: artists => {
         this.artists = artists;
       },
