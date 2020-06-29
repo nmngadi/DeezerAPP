30c0bc99327c46ad12b6a7cb0012f24ff0a41c19
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-06-29 23:00:32 +0200

    Add unit test short number pipe

--- src/app/short-number.pipe.spec.ts
+++ src/app/short-number.pipe.spec.ts
@@ -1,8 +1,8 @@
 import { ShortNumberPipe } from './short-number.pipe';
 
 describe('ShortNumberPipe', () => {
-  it('create an instance', () => {
+  it('should convert mumber to short number', () => {
     const pipe = new ShortNumberPipe();
-    expect(pipe).toBeTruthy();
+    expect(pipe.transform(1000)).toEqual('1K');
   });
 });
